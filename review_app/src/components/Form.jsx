dd560ae4c6efe68d37955cb2a156e2d16131a0e4
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2023-10-06 16:47:28 +0100

    Update_Form_Component

--- review_app/src/components/Form.jsx
+++ review_app/src/components/Form.jsx
@@ -1,18 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
+    const [name, setName] = useState('');
+    const [message,setMessage] = useState('');
+
+
     return (
         <form className='mt-5' >
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            <div className='mb-3'>
+                <label htmlFor='name' className='form-label'>Name</label>
+                <input 
+                    type='text' 
+                    name='name'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className='form-control'
+                    placeholder='Name'
+                    
+                />
             </div>
-            <div className="mb-3">
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                <textarea className="form-control" id="exampleFormControlTextarea1" rows={3} defaultValue={""} />
+            <div className='mb-3'>
+                <label htmlFor='message' className='form-label'>Message</label>
+                <textarea 
+                    
+                    name='message' 
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    className='form-control' 
+                    placeholder='Message'
+                    rows={3} 
+                    // defaultValue={''} 
+                />
             </div>
             <div className='col-auto'>
-                <button type="submit" className="btn btn-primary mb-3">Submit</button>
+                <button type='submit' className='btn btn-primary mb-3'>Submit</button>
             </div>
         </form>
     )
