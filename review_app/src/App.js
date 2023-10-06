a9fe5aee4d5d469d9405f71e420344346b06281e
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2023-10-06 16:24:03 +0100

    Create_Header_And_Form_Component

--- review_app/src/App.js
+++ review_app/src/App.js
@@ -1,7 +1,19 @@
+import Form from "./components/Form";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="row my-4" >
+      <div className="col-md-8 mx-auto">
+        <div className="card">
+          <div className="card-header bg-white">
+            <Header />
+          </div>
+          <div className="card-body" >
+            <Form />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
